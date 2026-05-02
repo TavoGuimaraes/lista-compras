442a5012ee9c66d7a5ae2091ff3a849c8e5371a6
TavoGuimaraes <luizgustavoguimaraes@gmail.com>
2026-05-02 20:56:44 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,28 @@
-const CACHE_NAME = 'lista-compras-v1';
+const CACHE_NAME = 'lista-compras-v2';
 const FILES_TO_CACHE = [
-  './',
-  './index.html',
-  './manifest.json'
+  '/lista-compras/',
+  '/lista-compras/index.html',
+  '/lista-compras/manifest.json',
+  '/lista-compras/icon-192.png',
+  '/lista-compras/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+      .then(() => self.skipWaiting())
   );
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', event => {
